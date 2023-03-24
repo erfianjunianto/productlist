1193c7d571c26da20859442aa5045b984ec81eed
erfianjunianto <erfianjunianto90@gmail.com>
2023-03-24 21:06:54 +0700

    update index page

--- App.tsx
+++ App.tsx
@@ -1,118 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+// 1. import `NativeBaseProvider` component
+import { NativeBaseProvider, Text, Box, Button, Checkbox, StatusBar,
+  HStack,
+  IconButton, Icon,
+  Center } from "native-base";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  // 2. Use at the root of your app
+  function AppBar() {
+    return <>
+        <StatusBar bg="#3700B3" barStyle="light-content" />
+        <Box safeAreaTop bg="violet.600" />
+        <HStack bg="violet.800" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
+          <HStack alignItems="center">
+            <IconButton icon={<Icon size="sm" as={MaterialIcons} name="menu" color="white" />} />
+            <Text color="white" fontSize="20" fontWeight="bold">
+              Home
+            </Text>
+          </HStack>
+          <HStack>
+            <IconButton icon={<Icon as={MaterialIcons} name="favorite" size="sm" color="white" />} />
+            <IconButton icon={<Icon as={MaterialIcons} name="search" size="sm" color="white" />} />
+            <IconButton icon={<Icon as={MaterialIcons} name="more-vert" size="sm" color="white" />} />
+          </HStack>
+        </HStack>
+      </>;
+  }
+  
+  function Example() {
+    return <Center>
+        <AppBar />
+      </Center>;
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <NativeBaseProvider>
+      <AppBar />
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button >Click Me</Button>
+        <Checkbox value="test" accessibilityLabel="This is a dummy checkbox" />
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
