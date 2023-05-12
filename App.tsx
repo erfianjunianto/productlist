ae067ddba9b5427f43f9979e2557d55eec176858
erfianjunianto <erfianjunianto90@gmail.com>
2023-05-12 20:05:54 +0700

    update icon

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 // 1. import `NativeBaseProvider` component
-import { NativeBaseProvider, Text, Box, Button, Checkbox, StatusBar,
+import { 
+  NativeBaseProvider, 
+  Text, 
+  Box, 
+  Button, 
+  Checkbox, 
+  StatusBar,
   HStack,
-  IconButton, Icon,
+  IconButton,
   Center } from "native-base";
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   // 2. Use at the root of your app
@@ -11,26 +19,16 @@ export default function App() {
     return <>
         <StatusBar bg="#3700B3" barStyle="light-content" />
         <Box safeAreaTop bg="violet.600" />
-        <HStack bg="violet.800" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
+        <HStack bg="violet.600" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%">
           <HStack alignItems="center">
-            <IconButton icon={<Icon size="sm" as={MaterialIcons} name="menu" color="white" />} />
+            <IconButton icon={<Icon name="ios-reorder-three" size={30} color="#FFF" />} />
             <Text color="white" fontSize="20" fontWeight="bold">
               Home
             </Text>
-          </HStack>
-          <HStack>
-            <IconButton icon={<Icon as={MaterialIcons} name="favorite" size="sm" color="white" />} />
-            <IconButton icon={<Icon as={MaterialIcons} name="search" size="sm" color="white" />} />
-            <IconButton icon={<Icon as={MaterialIcons} name="more-vert" size="sm" color="white" />} />
+  
           </HStack>
         </HStack>
       </>;
-  }
-  
-  function Example() {
-    return <Center>
-        <AppBar />
-      </Center>;
   }
   return (
     <NativeBaseProvider>
